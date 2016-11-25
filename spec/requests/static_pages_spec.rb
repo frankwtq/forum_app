5d69127385f99eee4d21b7df10364c3dc7760ffd
wangtianqi <wangtianqi@T4F-MAM-13097.local>
2016-11-25 17:25:27 +0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,26 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
+
     it "should have the content 'Sample Forum'" do
 	    visit '/static_pages/home'
 	    expect(page).to have_content('Sample Forum')
     end
+    it "should have the title 'Home'" do 
+	    visit '/static_pages/home'
+	    expect(page).to have_title("Sample Forum | Home")
+    end
   end
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+	    visit '/static_pages/help'
+	    expect(page).to have_content('Help')
+    end
+    it "should have the title 'Help'" do
+	    visit '/static_pages/help'
+	    expect(page).to have_title("Sample Forum | Help")
+    end
+  end
+  
 end
