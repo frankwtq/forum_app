4f962ddc45ee4a27670e95901d5ef32c56baf728
wangtianqi <wangtianqi@T4F-MAM-13097.local>
2016-11-25 15:31:49 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ForumApp::Application.config.secret_key_base = '381c1a9efa9c5984d6078e97e1f214101a11efd187ecd4d1dbf9a58742ef0814e1df7cd699bab226d0ef592e3f387933e0799f9387f5f44e93bc2c0618246c41'
