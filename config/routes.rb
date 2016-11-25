6e8ce2ab9e6edee1d149c6d689772be29cb35f74
wangtianqi <wangtianqi@T4F-MAM-13097.local>
2016-11-25 18:42:50 +0800

    Add Users controller, named /users/new to signup_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ForumApp::Application.routes.draw do
+  get "users/new"
 	root to: 'static_pages#home'
+	match '/signup',	to: 'users#new',		via: 'get'
 	match '/help',		to: 'static_pages#help',	via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
