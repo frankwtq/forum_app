ed170930f0c81b8ab772fa135e82665deedf81a4
wangtianqi <wangtianqi@T4F-MAM-13097.local>
2016-11-28 15:09:27 +0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,8 @@ describe User do
 	it {should respond_to(:password_digest)}
 	it {should respond_to(:password)}
 	it {should respond_to(:password_confirmation)}
+	it {should respond_to(:remember_token)}
+	it {should respond_to(:authenticate)}
 
 	it {should be_valid}
 	describe "when name is not present" do
@@ -73,5 +75,9 @@ describe User do
 		describe "with invalid password" do
 			it {should_not eq found_user.authenticate("invalid")}
 		end
+	end
+	describe "remember token" do
+		before {@user.save}
+		its(:remember_token) {should_not be_blank}
 	end
 end
